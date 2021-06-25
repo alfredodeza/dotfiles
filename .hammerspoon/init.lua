f9e9e57f9554dad1e706fd1966b45157d7f3d679
Alfredo Deza <alfredo@deza.pe>
2021-06-25 10:28:36 -0400

    add spoonInstall and fix window resizing

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -21,8 +21,37 @@ hs.hotkey.bind(mash, 'k', function() hs.window.focusedWindow():move(units.top50,
 hs.hotkey.bind(mash, 'j', function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
 hs.hotkey.bind(mash, 'y', function() hs.window.focusedWindow():move(units.upleft30,   nil, true) end)
 hs.hotkey.bind(mash, 'u', function() hs.window.focusedWindow():move(units.botleft30,  nil, true) end)
-hs.hotkey.bind(mash, 'o', function() hs.window.focusedWindow():move(units.botright30,   nil, true) end)
+hs.hotkey.bind(mash, 'o', function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
 hs.hotkey.bind(mash, "i", function() hs.window.focusedWindow():move(units.upright30,  nil, true) end)
 hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
-hs.hotkey.bind(mash, ']', function() hs.window.focusedWindow():move(units.center,    nil, true) end)
+hs.hotkey.bind(mash, ']', function() hs.window.focusedWindow():move(units.center,     nil, true) end)
 
+
+hs.loadSpoon("SpoonInstall")
+Install=spoon.SpoonInstall
+
+Install:andUse("KSheet",
+               {
+                 hotkeys = {
+                   toggle = { mash, "/" }
+}})
+
+Install:andUse("MouseCircle", { hotkeys = { show = { mash, "=" }}})
+
+Install:andUse("Seal",
+               {
+                 hotkeys = { show = { mash, "space" } },
+                 fn = function(s)
+                   s:loadPlugins({"apps", "useractions"})
+                   s.plugins.useractions.actions =
+                     {
+			    ["Red Hat Bugzilla"] = { url = "https://bugzilla.redhat.com/show_bug.cgi?id=${query}", icon="favicon", keyword="bz" },
+			    ["Red Hat Support"] = { url = "https://access.redhat.com/support/cases/#/case/${query}", icon="favicon", keyword="sup" },
+			    ["Red Hat Support Exception"] = { url = "https://tools.apps.cee.redhat.com/support-exceptions/id/${query}", icon="favicon", keyword="se" },
+			    ["Launchpad Bugs"] = { url = "https://launchpad.net/bugs/${query}", icon="favicon", keyword="lp" },
+                     }
+                   s:refreshAllCommands()
+                 end,
+                 start = true,
+               }
+)
